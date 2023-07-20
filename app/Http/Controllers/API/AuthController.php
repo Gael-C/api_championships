2a9b00a236e068ebcc66daba9e921235c5117b8c
Gael <gael_coupe17@outlook.fr>
2023-07-20 11:11:24 +0200

    Mise en place de l'auth

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+
+       $validated = $request->validate([
+        'name' => 'required|string',
+        'email' => 'required|email',
+        'password' => 'required'
+       ]);
+
+       $user = User::create([
+        'name' => $validated['name'],
+        'email' => $validated['email'],
+        'password' => bcrypt($validated['password'])
+       ]);
+
+       $token = $user->createToken('myToken')->plainTextToken;
+
+       return new JsonResponse([
+        'token' => $token,
+        'token-type' => 'Bearer'
+       ], 201);
+    }
+
+    public function login(Request $request) {
+        
+        if(!Auth::attempt($request->only('email', 'password'))) {
+            return new JsonResponse(['message' => 'Login invalide'], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('myToken')->plainTextToken;
+       
+        return new JsonResponse([
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\API\LeaguesController;
 use App\Http\Controllers\API\PicturesController;
 use App\Http\Controllers\API\TeamsController;
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,9 +17,29 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// Routes protégées
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResources([
+        'teams' => TeamsController::class,
+        'leagues' => LeaguesController::class,
+        'pictures' => PicturesController::class
+    ]);
+});
 
-Route::apiResources([
-    'teams' => TeamsController::class,
-    'leagues' => LeaguesController::class,
-    'pictures' => PicturesController::class
-]);
+// Routes publiques
+Route::get('/teams', [TeamsController::class, 'index']);
+Route::get('/teams/{team}', [TeamsController::class, 'show']);
+
+Route::get('/leagues', [LeaguesController::class, 'index']);
+Route::get('/leagues/{league}', [LeaguesController::class, 'show']);
+
+Route::get('/pictures', [PicturesController::class, 'index']);
+Route::get('/pictures/{picture}', [PicturesController::class, 'show']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+
+
+
+
