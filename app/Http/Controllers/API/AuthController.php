4e9e755d0e83a0a2b0cf62c46748fe715bb466a9
Gael <gael_coupe17@outlook.fr>
2023-07-21 10:15:25 +0200

    Ajout protection création de compte

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
 
        $validated = $request->validate([
         'name' => 'required|string',
-        'email' => 'required|email',
+        'email' => 'required|email|unique:users',
         'password' => 'required'
        ]);
 
